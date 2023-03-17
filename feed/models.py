fbb8b2ad99adaf31806a59856411227b9418e71a
Gaurav <gauravsomani52750@gmail.com>
2023-03-17 19:09:43 +0530

    created student model

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
+
 # Create your models here.
+
+
+class Student(models.Model):
+    # 1-1 field (Student - User)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    birth_date = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=255)
+    gender = models.CharField(max_length=15)
+    college = models.TextField()
+    college_city = models.CharField(max_length=100)
+    current_year = models.CharField(max_length=15)
+    branch = models.CharField(max_length=100)
+    about_me = models.TextField()
+    skills = ArrayField(models.CharField(max_length=50))
