638fe63955ca6ee8628069e711b3036832b8e4b5
Gaurav <gauravsomani52750@gmail.com>
2023-03-19 03:34:12 +0530

    url bug

--- feed/urls.py
+++ feed/urls.py
@@ -10,10 +10,10 @@ router.register('comment', views.CommentView)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('student/<str:username>',
+    path('student/<str:username>/',
          views.StudentProfileView.as_view(), name='student-profile'),
     path('post/', views.AllPost.as_view(), name='all-posts'),
     path('post/like/', views.like_post),
-    path('post/comment/<int:pk>', views.comment_on_post),
+    path('post/comment/<int:pk>/', views.comment_on_post),
     path('student/recommendations/', views.recommendations),
 ]
