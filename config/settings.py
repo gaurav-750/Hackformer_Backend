5cd76d8888c7f0802efc087489c8cb34b409cebe
Gaurav <gauravsomani52750@gmail.com>
2023-03-18 11:31:46 +0530

    installed drf, djoser

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import environ
 
@@ -43,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party
-
+    'rest_framework',
+    'djoser',
 
     # local
     'core',
@@ -139,3 +141,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
+    }
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+}
--- feed/admin.py
+++ feed/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Student, Post
+from .models import Comment, Student, Post
 
 # Register your models here.
 admin.site.register(Student)
 
 admin.site.register(Post)
+
+admin.site.register(Comment)
