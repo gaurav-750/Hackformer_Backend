17c90dca61c1b0608e6a6ddc28642ddb5724d8a9
Gaurav <gauravsomani52750@gmail.com>
2023-03-19 00:18:48 +0530

    added whitenoise

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,6 +87,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# *this is the endpt, at which we want to expose our user uploaded files:
+MEDIA_URL = '/media/'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
