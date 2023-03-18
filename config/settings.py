b0c69956d7601ceb85943bce8be1ac56507025ff
Gaurav <gauravsomani52750@gmail.com>
2023-03-18 23:27:58 +0530

    deployed postgresql database, other settings for deploying backend

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import environ
+import dj_database_url
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -82,19 +84,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-    }
+    'default': dj_database_url.parse(env("DATABASE_URL"))
 }
 
 
